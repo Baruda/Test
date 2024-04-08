22828ead29e3343e39255489f3763aee141e1029
Francis E. Ibok <francis_ibok@yahoo.com>
2024-04-08 17:15:36 +0100

    Completed the Tech App

---
+++ Techie News/Techie_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Techie_NewsApp.swift
+//  Techie News
+//
+//  Created by Francis Ibok on 08/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Techie_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Techie News/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  Techie News
+//
+//  Created by Francis Ibok on 08/04/2024.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
