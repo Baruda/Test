825094120f76529fd781c9f28639b6a1e1892f7a
Baruda <francis.ibok@gmail.com>
2024-04-09 05:27:07 +0100

    Add files via upload

---
+++ ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Techie News
+//
+//  Created by Francis Ibok on 08/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("TECH NEWS")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
---
+++ PostData.swift
@@ -0,0 +1,22 @@
+//
+//  PostData.swift
+//  Techie News
+//
+//  Created by Francis Ibok on 08/04/2024.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let points: Int
+    let title: String
+    let url: String?
+}
